6a3a182212aa1dd9401f5396a092f2b5139486f0
남의영 <ny2485@naver.com>
2023-03-25 19:08:59 +0900

    feat : Card클래스 추가

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -1,2 +1,21 @@
-package blackjack.domain;public class CARD {
+package blackjack.domain;
+
+public class Card {
+    Denomination denomination;
+    Suit suit;
+
+    public Card(Denomination denomination, Suit suit) {
+
+        this.denomination = denomination;
+        this.suit = suit;
+    }
+
+
+    public Denomination getDenomination() {
+        return denomination;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
 }
