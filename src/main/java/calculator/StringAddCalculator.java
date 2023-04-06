f0e660b87c3f40088a9257048470dde0ab8c7324
남의영 <ny2485@naver.com>
2023-04-07 01:12:38 +0900

    feat : 문자열 null or 0 검증

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -7,6 +7,17 @@ import java.util.stream.Collectors;
 public class StringAddCalculator {
     public static final String SEPERATOR = ",|:";
     public static Integer makeNumberToSum(String str) {
+        if (!isValidStr(str)) {
+            return 0;
+        }
         return Arrays.stream(str.split(SEPERATOR)).mapToInt(Integer::parseInt).sum();
+    }
+
+
+    public static boolean isValidStr(String str) {
+        if (str == null | str == "") {
+            return false;
+        }
+        return true;
     }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringAddCalculatorTest {
 
     // 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환 (예: “” => 0, "1,2" => 3, "1,2,3" => 6, “1,2:3” => 6)
@@ -17,7 +19,18 @@ public class StringAddCalculatorTest {
         String str = "1,2:5,4";
 
         Integer sumNum = StringAddCalculator.makeNumberToSum(str);
-        Assertions.assertThat(sumNum).isEqualTo(12);
+        assertThat(sumNum).isEqualTo(12);
+    }
+    @Test
+    void 구분자값이_없을때_숫자의_합() {
+        String str = "";
+        Integer sumNum = StringAddCalculator.makeNumberToSum(str);
+        assertThat(sumNum).isEqualTo(0);
+    }
 
+    @Test
+    void 문자열_null_zero_검증() {
+        String str = "";
+        assertThat(StringAddCalculator.isValidStr(str)).isFalse();
     }
 }
