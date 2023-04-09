55a68fccc71f88c0f9be1cd38ef42ff88f2fa158
남의영 <ny2485@naver.com>
2023-04-10 03:00:16 +0900

    feat : input과 output View코드 추가

--- src/main/java/racingGame/view/OutputView.java
+++ src/main/java/racingGame/view/OutputView.java
@@ -1,2 +1,28 @@
-package racingGame.view;public class OutputView {
+package racingGame.view;
+
+import racingGame.domain.Car;
+import racingGame.domain.CarMoveStrategy;
+import racingGame.domain.Cars;
+
+public class OutputView {
+
+    public static void printResults(int attemptLimit, Cars cars) {
+        System.out.println("실행결과");
+        for (int i = 0; i < attemptLimit; i++) {
+            printOneResult(cars);
+            System.out.println();
+        }
+    }
+
+    private static void printOneResult(Cars cars) {
+        for (Car car : cars.getCarList()) {
+            car.move(new CarMoveStrategy());
+            System.out.println(String.format("%s : %s", car.getCarName(), "-".repeat(car.getCarDist())));
+        }
+    }
+
+    public static void printWinners(Cars cars) {
+        String winners = cars.getWinners();
+        System.out.println(String.format("%s가 최종 우승했습니다.", winners));
+    }
 }
