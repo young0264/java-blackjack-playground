1c646d6ba826f19d46ffa9ea0880608163281e0f
남의영 <ny2485@naver.com>
2023-04-16 23:34:53 +0900

    add : Random Test

--- src/test/java/racingGame/domain/CarMoveStrategyTest.java
+++ src/test/java/racingGame/domain/CarMoveStrategyTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
-class CarMoveStrategyTest {
-  
+package racingGame.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+
+class CarMoveStrategyTest implements MoveStrategy {
+    public static final int MIN_RANDOM_NUM = 4;
+
+    @Override
+    public int createMovePoint() {
+        return 5;
+    }
+
+    @Test
+    public void 랜덤값이_범위안일때() {
+        Assertions.assertThat(createMovePoint() >= MIN_RANDOM_NUM).isTrue();
+    }
+
+
 }
