591dc658a293a9d3f606333081dddf9f2b61d3e7
남의영 <ny2485@naver.com>
2023-04-22 11:05:49 +0900

    feat: 사각형 테스트,클래스 추가

--- src/main/java/coordinate/domain/Rectangle.java
+++ src/main/java/coordinate/domain/Rectangle.java
@@ -1,2 +1,44 @@
-package coordinate.domain;public class Rectangle {
+package coordinate.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Rectangle {
+
+    List<Point> pointList;
+
+    public Rectangle(List<Point> pointList) {
+        this.pointList = pointList;
+    }
+
+    public double pointResult() {
+
+        Set<Integer> collectX = getUniqueCoordinateX(pointList);
+        Set<Integer> collectY = getUniqueCoordinateY(pointList);
+
+        return calculatePointLength(collectX) * calculatePointLength(collectY);
+    }
+
+    private double calculatePointLength(Set<Integer> collectCoordinates) {
+        List<Integer> values = new ArrayList<>(collectCoordinates);
+        return Math.abs(values.get(0) - values.get(1));
+    }
+
+    private Set<Integer> getUniqueCoordinateX(List<Point> pointList) {
+        return getUniqueCoordinates(pointList, Point::getX);
+    }
+
+    private Set<Integer> getUniqueCoordinateY(List<Point> pointList) {
+        return getUniqueCoordinates(pointList, Point::getY);
+    }
+
+    private Set<Integer> getUniqueCoordinates(List<Point> pointList, Function<Point, Integer> function) {
+        return pointList.stream()
+                .map(function)
+                .collect(Collectors.toSet());
+    }
+
 }
